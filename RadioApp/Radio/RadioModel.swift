d649ed444b9c883d3d718731d73e2b70ecff5e95
Johansson <inste@icloud.com>
2019-01-20 22:09:11 +0200

    fifteenth push

--- RadioApp/Radio/RadioModel.swift
+++ RadioApp/Radio/RadioModel.swift
@@ -16,8 +16,9 @@ class Radio {
     let informatorFM = AVPlayer(url: URL(string: "http://media.fregat.com:8000/RadioMIX.m3u")!)
     let kissFM = AVPlayer(url: URL(string: "https://www.kissfm.ua/KissFM.m3u")!)
     let luxFM = AVPlayer(url: URL(string: "http://lux.fm/resources/file/music/lux.m3u")!)
+    let shansonFM = AVPlayer(url: URL(string: "http://media2.brg.ua:8000/shanson_h.m3u")!)
     
-    let ukraineRadio = ["Informator FM", "Kiss FM", "Lux FM"]
+    let ukraineRadio = ["Informator FM", "Kiss FM", "Lux FM", "Hit FM", "Shanson FM"]
     
 
 }
